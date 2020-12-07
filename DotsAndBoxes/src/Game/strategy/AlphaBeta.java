455f02a040ba75e4689590e452ee86a14939dbeb
elifozkn <elifozkan2000@gmail.com>
2020-12-07 09:35:11 +0100

    alpha beta start

--- DotsAndBoxes/src/Game/strategy/AlphaBeta.java
+++ DotsAndBoxes/src/Game/strategy/AlphaBeta.java
@@ -9,17 +9,100 @@ import java.util.ArrayList;
 
 public class AlphaBeta extends GameStrategy{
 
+    private int maxDepth;
+    private  int MIN = -1000000000;
+    private int MAX = 1000000000;
+
     public AlphaBeta(String title, boolean isPlayer1, Color color) {
         super(title, isPlayer1, color);
     }
 
-    public WeightedEdge initialize(DummyBoard board,int depth, int alpha, int beta){
-        ArrayList<Edge> moves = board.getMoves();
-        if(moves.size() == 0){
-            return new WeightedEdge(null, evaluationFunction(board));
+    public int alphaBeta(DummyBoard b, int depth, Boolean maximizingPlayer,int alpha,int beta) {
+
+        if (depth == 0 || b.getMoves().size()==0) {//Depth reached or game is over.{
+            return b.calculateScorePlayer2()-b.calculateScorePlayer1();
         }
-        ArrayList<DummyBoard> children = board.getChildren();
-		return null;
+
+        if (maximizingPlayer) {//AI
+            int maxEva = -2147483647;//Negative infinity
+            int score = 0;
+            //Checks every possible move AI can make
+            ArrayList<Edge> moves = b.getMoves();
+            DummyBoard temp;
+            for(Edge edge : moves){
+                temp= new DummyBoard(b.n);
+                temp.copyGameBoard(b);
+                temp.processAIMove2(edge);
+                if (!temp.getCurrentPlayer().isPlayer1) {
+                    score = alphaBeta(temp, depth-1 , true,alpha,beta);
+                }else {
+                    score = alphaBeta(temp, depth - 1, false,alpha,beta);
+                }
+                maxEva = Math.max(maxEva, score);
+
+            }
+            return maxEva;
+
+        }
+        else {//Players turn calculation
+
+            int minEva = 2147483647;//Positive infinity
+            int score = 0;
+            //Checks every possible move Human player can make
+            ArrayList<Edge> moves = b.getMoves();
+            DummyBoard temp;
+
+            for(Edge edge : moves){
+                temp= new DummyBoard(b.n);
+                temp.copyGameBoard(b);
+                temp.processAIMove2(edge);
+                if (temp.getCurrentPlayer().isPlayer1) {
+                    score = alphaBeta(temp, depth -1, false,alpha,beta);
+                }
+                else {
+                    score = alphaBeta(temp, depth - 1, true,alpha,beta);
+                }
+                minEva = Math.min(minEva, score);
+            }
+
+            return minEva;
+        }
+
+
+    }
+
+    public Edge AlphaBetaStart(DummyBoard board,int depth,int alpha , int beta) {
+        Edge pos=null;
+        int max = -2147483647;
+        int eval = 0;
+
+        ArrayList<Edge> moves = board.getMoves();
+        DummyBoard temp;
+
+        for(Edge edge : moves){
+            temp= new DummyBoard(board.n);
+            temp.copyGameBoard(board);
+            //temp.fillEdge(edge);
+            temp.processAIMove2(edge);
+            if (!temp.getCurrentPlayer().isPlayer1) {
+                eval = alphaBeta(temp, depth, true,alpha,beta);
+            }else {
+                eval = alphaBeta(temp, depth, false,alpha,beta);
+            }
+
+            if (eval > max) {
+                max = eval;
+                pos = edge;
+            }
+        }
+        //System.out.println("Best move x : " + pos.getX() +  "y : " + pos.getY() );
+        return pos;
+    }
+    @Override
+    public Edge makeMove(DummyBoard gameBoard) {
+        maxDepth = 3;
+        return AlphaBetaStart(gameBoard, maxDepth,MIN,MAX);
+
 
     }
 
