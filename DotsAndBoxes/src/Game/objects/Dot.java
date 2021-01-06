4b7edf4cb2351bda2124bf225cdcdace88bd7c4e
elifozkn <elifozkan2000@gmail.com>
2021-01-06 10:54:46 +0300

    Revert "clean unnecessary files"
    This reverts commit 770e6a703559d611c7dc71372511cae276d26a69.

---
+++ DotsAndBoxes/src/Game/objects/Dot.java
@@ -0,0 +1,99 @@
+
+
+/**
+ * class that represent a dot component of the game, represented as a vertex of a graph
+ */
+public class Dot {
+   private int posX, posY; //position of the dot on the gameboard
+    private boolean isOccupied;
+
+    /**
+     * constructor
+     * @param posX is the x-coordinate of the dot on the game grid
+     * @param posY y-coordinate of the dot on the game grid
+     */
+    public Dot(int posX, int posY){
+        this.posX = posX;
+        this.posY = posY;
+        this.isOccupied = false;
+    }
+
+    /**
+     * accessor method
+     * @return x position of the dot
+     */
+    public int getPosX(){
+        return this.posX;
+    }
+
+    /**
+     * accesor method
+     * @return y-position of the dot
+     */
+    public int getPosY(){
+        return this.posY;
+    }
+
+
+
+    /**
+     * to check if a dot is within the boundaries
+      * @param lower is the lower dots
+     * @param upper upper dot
+     * @return
+     */
+    public boolean isWithin(Dot lower, Dot upper){
+
+        return posX >= lower.posX && posX <= upper.posX
+                && posY >= lower.posY && posY <= upper.posY;
+    }
+
+
+    /**
+     * checks if a dot can be joined with other dot vertically
+     * @param d2 other dot
+     * @return true if they can be joined vertically
+     */
+    private boolean formsVerticalLine(Dot d2){
+        if(this.posX == d2.posX){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * checks if a dot can be joined with other dot horizontally
+     * @param d2 other dot
+     * @return true if they can be joined horizontally
+     */
+    private boolean formsHorizontalLine(Dot d2){
+        if(this.posY == d2.posY){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * checks if a dot can be joined with another dot
+     * @param d2 is another dot
+     * @return true if they can be joined
+     */
+    public boolean canBeJoined(Dot d2){
+        if(this == d2){
+            return false;
+        } else if(formsHorizontalLine(d2) || formsVerticalLine(d2)){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isOccupied(){
+        return this.isOccupied;
+    }
+
+    public void setOccupied(boolean o){
+        this.isOccupied = o;
+    }
+
+
+}
---
+++ DotsAndBoxes/src/Game/objects/Graph.java
@@ -0,0 +1,104 @@
+/*
+package Game.objects;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+*/
+/**
+ * class that represents a graph that contains dots and lines
+ *//*
+
+public class Graph {
+
+    private Map<Dot, LinkedList<Dot>> adj; //adjacency map which will store the adjacent dots
+    private List<Line> lines;
+
+    */
+/**
+     * constructor method
+     * @param lines contain the lines(edges) within the graph
+     *//*
+
+    public Graph(List<Line> lines){
+        this.lines = lines;
+        adj = new HashMap<>();
+
+        //initialize the adjacency map
+        for(int i = 0; i < lines.size() ; i++){
+            adj.put(lines.get(i).getSource() , new LinkedList<Dot>());
+        }
+
+        for(int i = 0; i < lines.size(); i++){
+            adj.get(lines.get(i).getSource()).add(lines.get(i).getDest());
+        }
+    }
+
+
+    */
+/**
+     * accessor method for the lines
+     * @return the list of the lines
+     *//*
+
+    public List<Line> getLines(){
+        return this.lines;
+    }
+
+    */
+/**
+     * accessor method for the adjacent dots of a chosen dot
+     * @param dot is the particular dot
+     * @return a list containing adjacent dots to the parameter dot.
+     *//*
+
+    public List<Dot> getAdjacents(Dot dot){
+        return adj.get(dot);
+    }
+
+    */
+/**
+     * helper method for addLine method
+     * @param source is the source vertex
+     * @param dest destination vertex
+     * @return true if the edge can be added
+     *//*
+
+    private boolean addEdge(Dot source, Dot dest,boolean vertical ){
+        if(!adj.containsKey(source)){
+            adj.put(source,new LinkedList<>());
+        }
+
+        adj.get(source).add(new Line(source,dest,vertical).getDest());
+        return true;
+    }
+
+    */
+/**
+     * method to add lines in the game graph
+     * @param source is the start point of the dot
+     * @param dest endpoint of the dot
+     * @return true if the line is added
+     *//*
+
+    public boolean addLine(Dot source, Dot dest,boolean vertical){
+        addEdge(source,dest, vertical);
+        addEdge(dest,source,vertical);
+        return true;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        for(Line l : lines){
+            sb.append("(" + l.getSource() + " -- " + l.getDest() +  ")\t"  );
+
+        }
+        return sb.toString();
+    }
+
+
+}
+*/
